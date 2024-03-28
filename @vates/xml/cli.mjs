361e89bebba1300487014c7d3a7554f1ec6f11f3
Julien Fontanet <julien.fontanet@isonoe.net>
2024-03-28 15:12:54 +0100

    WiP

--- @vates/xml/cli.mjs
+++ @vates/xml/cli.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { inspect } from 'node:util'
+import { format } from '@vates/xml/format.mjs'
 import { parse } from '@vates/xml/parse.mjs'
 import { readFileSync } from 'node:fs'
 
@@ -9,7 +10,19 @@ function log(val) {
   process.stdout.write('\n')
 }
 
-function main([input = 0]) {
-  log(parse(readFileSync(input)))
+function main([inputPath = 0]) {
+  const input = readFileSync(inputPath)
+
+  // attempt to parse from JSON
+  let data
+  try {
+    data = JSON.parse(input)
+  } catch (error) {
+    // fallback to XML
+    log(parse(input))
+    return
+  }
+
+  process.stdout.write(format(data))
 }
 main(process.argv.slice(2))
